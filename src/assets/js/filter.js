24c4dee61eed27ff52e2bbe161182a0daa61d523
Louis Ritchie <louiscritchie@gmail.com>
2018-11-18 22:16:56 -0800

    have a selected status showing

--- src/assets/js/filter.js
+++ src/assets/js/filter.js
@@ -1,18 +1,18 @@
 let currentTag;
+let lastRef;
 let projects = [];
 
 const showProject = (project) => {
   const nextProject = project;
   nextProject.hidden = false;
-  document.getElementById(nextProject.id).style = 'display: inherit;';
+  nextProject.el.style = 'display: inherit;';
   return nextProject;
 };
 
 const hideProject = (project) => {
-  console.log('hiding project ', project)
   const nextProject = project;
   nextProject.hidden = true;
-  document.getElementById(nextProject.id).style = 'display: none;';
+  nextProject.el.style = 'display: none;';
   return nextProject;
 };
 
@@ -36,26 +36,34 @@ function filterProjects() {
     });
 }
 
-const handleClickFactory = tag => () => {
+const handleClickFactory = (tag, ref) => () => {
   if (currentTag === tag) {
     currentTag = undefined;
     showAll();
+    $(ref).removeClass('selected');
   } else {
     currentTag = tag;
     filterProjects();
+    if (lastRef) {
+      $(lastRef).removeClass('selected');
+    }
+    $(ref).addClass('selected');
   }
+
+  lastRef = ref;
 };
 
 function main() {
   // assign the click handlers to the filter buttons
   for (const filterButton of document.getElementsByClassName('projects-filter__tag')) {
     const filter = filterButton.getAttribute('data-tag');
-    filterButton.addEventListener('click', handleClickFactory(filter));
+    filterButton.addEventListener('click', handleClickFactory(filter, filterButton));
   }
 
+  // populate the projects global state object by reading the DOM
   for (const project of document.getElementsByClassName('project-thumbnail__link')) {
     projects.push({
-      id: project.getAttribute('id'),
+      el: project,
       tags: project.getAttribute('data-tags').split(','),
       hidden: false,
     });
